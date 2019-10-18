4ee478122cce3f77f4a62caa5787b3ce715fbf0b
quentin <quentinrougemont@orange.fr>
2019-10-18 17:10:53 +0200

    edited rename_file.sh

--- rename_file.sh
+++ rename_file.sh
@@ -6,10 +6,14 @@ input=$1  #shared name of the list of file
 rename=$2 #basename to replace 
 newname=$3 #replacement name
      
-for i in $( ls *$input* ); 
+for i in *"$input"*; 
 do
 	#echo -e "
 	input=$i
 	output=$(echo "$input" | sed -e "s/$rename/$newname/g")
 	mv "$input" "$output" #"
 done
+
+#or simply use the function rename:
+#rename 's/from/to/' pattern 
+#beware that you need the one by Robin Barker in perl  
