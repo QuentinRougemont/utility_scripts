5bf49e4769fde843d612450bcdcc0dff86569aa5
quentin <quentinrougemont@orange.fr>
2020-02-17 10:38:48 +0100

    add script

---
+++ cont_mapped_read.sh
@@ -0,0 +1,11 @@
+
+
+#conting the number of successfully mapped read
+#for single end:
+for i in *bam ; do samtools view -F 0x904 -c $i |awk -v var="$i" 'END {print var"\t"$1}' ; done
+
+#paired-end:
+for i in *bam ; do 
+	samtools view -F 0x4 $i | cut -f 1 | sort | uniq | wc -l \
+		awk -v var="$i" 'END {print var"\t"$1}' ;
+done
