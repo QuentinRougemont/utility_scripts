03ce5bc169ebc41552f36fc8c69cefb43c76a8b6
Quentin <quentinrougemont@orange.fr>
2021-03-16 11:14:55 +0100

    add awk function

--- 07.random_scripts/awk_mean_and_sd_over_all.columns.sh
+++ 07.random_scripts/awk_mean_and_sd_over_all.columns.sh
@@ -2,8 +2,11 @@
 
 #compute mean and standard deviation over a matrix file 
 #output: 2 colomns file with mean\sd for each colomns of the file
+input=$1
 
 awk '{for(i=1;i<=NF;i++) {sum[i] += $i; sumsq[i] += ($i)^2}}
            END {for (i=1;i<=NF;i++) {
            printf "%f %f \n", sum[i]/NR, sqrt((sumsq[i]-sum[i]^2/NR)/NR)}
-          }' file
+          }' $input > mean_sd_by_columns.txt
+
+
--- 07.random_scripts/awk_sum_and_occurence.sh
+++ 07.random_scripts/awk_sum_and_occurence.sh
@@ -7,14 +7,14 @@
 awk -F '\t' '{a[$1] += $2} END{for (i in a) print i, a[i]}'  inputfile
 
 #exemple with Hmel2.5
-sed 's/[0-9]o//g' 03_genome/Hmel2.5_plusmtDNA.fa.fai |\
+sed 's/[0-9]o//g' Hmel2.5_plusmtDNA.fa.fai |\
     awk -F '\t' '{a[$1] += $2} END{for (i in a) print i, a[i]}' |\
     LC_ALL=C sort -k 2 -nr |less
 
 #sum de la longeur total et comptage des occurences:
 #equivalent dplyr de group_by(chr) %>% summarise(longeur(sum(V3), n())
 
-sed 's/[0-9]o//g' 03_genome/Hmel2.5_plusmtDNA.fa.fai | \
+sed 's/[0-9]o//g' Hmel2.5_plusmtDNA.fa.fai | \
     awk -F '\t' '{a[$1] += $2; b[$1]++;} 
     END{for (i in a) print i, a[i], b[i]}' |\
         LC_ALL=C sort -k 2 -nr |less
