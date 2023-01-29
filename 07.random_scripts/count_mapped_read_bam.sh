54d3a3c2fc8f4f1296716ab0442812376fea5d5a
Quentin Rougemont <quentinrougemont@orange.fr>
2023-01-29 21:19:39 +0100

    Update count_mapped_read_bam.sh

--- 07.random_scripts/count_mapped_read_bam.sh
+++ 07.random_scripts/count_mapped_read_bam.sh
@@ -6,6 +6,6 @@ for i in *bam ; do samtools view -F 0x904 -c $i |awk -v var="$i" 'END {print var
 
 #paired-end:
 for i in *bam ; do 
-	samtools view -F 0x4 $i | cut -f 1 | sort | uniq | wc -l \
+	samtools view -F 0x4 $i | cut -f 1 | sort | uniq | wc -l |\
 		awk -v var="$i" 'END {print var"\t"$1}' ;
 done
