ec430f7e3c7a258869c42c0c7c59adabc64a4edc
Quentin <quentinrougemont@orange.fr>
2023-02-12 12:17:15 +0100

    handle compression

--- 07.random_scripts/count_read_fastq.sh
+++ 07.random_scripts/count_read_fastq.sh
@@ -3,16 +3,26 @@ if [ $# -lt 1 ] ; then
     echo ""
     echo "usage: count_fastq.sh [fastq_file1] <fastq_file2> ..|| *.fastq"
     echo "counts the number of reads in a fastq file"
+    echo "fastq can be compressed or not"
     echo ""
     exit 0
 fi
- 
+
+#note: the compression check does not work on symlink files!
+
 fastq=${@};
 for i in ${fastq[@]}
 do
-lines=$(zcat $i |wc -l |cut -d " " -f 1)
-count=$(($lines / 4))
-echo -n -e "\t$i : "
-echo "$count"  
+	if file --mime-type "$i" | grep -q gzip$; then
+		lines=$(zcat $i |wc -l |cut -d " " -f 1)
+		count=$(($lines / 4))
+		echo -n -e "\t$i : "
+		echo "$count"  
+	else
+		lines=$(wc -l $i |cut -d " " -f 1)
+		count=$(($lines / 4))
+		echo -n -e "\t$i : "
+		echo "$count"  
+	fi
 done
 
